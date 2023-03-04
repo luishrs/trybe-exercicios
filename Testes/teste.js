7202117c4f0b568da0a4f502b53fee0a766c48aa
luishrs <luis-hrs@outlook.com>
2023-03-04 19:40:30 -0300

    Adicionado exercícios para fixar, for / in

--- Testes/teste.js
+++ Testes/teste.js
@@ -1,29 +1,7 @@
-// function verificaFimPalavra(palavra, final) {
-//     if (palavra.endsWith(final)) {
-//         return true;
-//     }
-//     return false;
-// }
-//  console.log(verificaFimPalavra('coração','aaaa')); 
+const person = {fname:"John", lname:"Doe", age:25};
 
-
-//  function verificaFimPalavra(palavra, final) {
-//     if (final.length > palavra.length) {  // verifica se palavra2 é maior do que palavra1
-//       return false;
-//     } else {
-//       for (let index = 1; index <= final.length; index+= 1) {
-//         if (palavra[palavra.length - index] !== final[final.length - index]) {  // compara caracteres a partir do final de palavra1
-//           return false;
-//         }
-//       }
-//       return true;
-//     }
-//   }
-//   console.log(verificaFimPalavra('coração','oção')); 
-  
-let car = {
-  type: 'Fiat',
-  model: '500',
-  color: 'white',
-};
-console.log(car['model']);
+let text = "";
+for (let x in person) {
+  text += person[2];
+}
+console.log(text[2]);
