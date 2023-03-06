e7c1d33e3ae61d5f37da82ec0c1630a3edc5fd07
luishrs <luis-hrs@outlook.com>
2023-03-06 00:32:38 -0300

    adicionado exercício bônus, 6 total de estudantes

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-bonus/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-bonus/scripts.js
@@ -21,22 +21,22 @@ function addShift(Object, key, value) {
     Object[key] = value;
 }
 addShift(lesson2, 'turno', 'noite');
-console.log(lesson2);
+// console.log(lesson2);
 
 function listKeys(Object, key) {
     return Object[key]
 }
-console.log(listKeys(lesson2, 'materia'));
+// console.log(listKeys(lesson2, 'materia'));
 
 function objectSize(obj) {
    return Object.keys(obj).length;
 }
-console.log(objectSize(lesson2));
+// console.log(objectSize(lesson2));
 
 function listKeys(obj) {
     return Object.values(obj)
 }
-console.log(listKeys(lesson1));
+// console.log(listKeys(lesson1));
 
 let allLessons = Object.assign({}, {
    lesson1: lesson1,
@@ -44,4 +44,15 @@ let allLessons = Object.assign({}, {
    lesson3: lesson3,    
 });
 
-console.log(allLessons);
+// console.log(allLessons);
+//Com base no objeto elaborado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+function numberOfStudants(obj) {
+    let totalNumber = 0;
+    let keys = Object.keys(obj);
+    for(index in keys){
+        totalNumber += obj[keys[index]].numeroEstudantes;
+    }
+    return totalNumber;
+}
+console.log(numberOfStudants(allLessons));
