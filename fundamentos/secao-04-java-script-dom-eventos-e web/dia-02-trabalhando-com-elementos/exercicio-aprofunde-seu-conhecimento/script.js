1ab2336b0028ec3694b518e92dff3af3133ca2eb
luishrs <luis-hrs@outlook.com>
2023-03-13 18:08:44 -0300

    foram arrumados, nomes de diretórios

--- fundamentos/secao-04-java-script-dom-eventos-e web/dia-02-trabalhando-com-elementos/exercicio-aprofunde-seu-conhecimento/script.js
+++ fundamentos/secao-04-java-script-dom-eventos-e web/dia-02-trabalhando-com-elementos/exercicio-aprofunde-seu-conhecimento/script.js
@@ -1,6 +1,7 @@
 
 const elementH1 = document.createElement('h1');
 elementH1.innerText = 'TrybeTrip - Agência de Viagens';
+elementH1.className = 'title';
 document.body.appendChild(elementH1);
 
 const elementMain = document.createElement('main');
@@ -30,19 +31,40 @@ leftSection.appendChild(elementImg);
 
 const listaNoOrden = document.createElement('ul');
 rigthSection.appendChild(listaNoOrden);
-const arrayNumbers= ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis',
-'Sete', 'Oito', 'Nove', 'Dez'];
-for(let num in arrayNumbers) {
+const arrayNumbers = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis',
+    'Sete', 'Oito', 'Nove', 'Dez'];
+for (let num in arrayNumbers) {
     const elementoLi = document.createElement('li');
     elementoLi.innerHTML = arrayNumbers[num];
     listaNoOrden.appendChild(elementoLi);
 }
 
-for(let index = 0; index <3; index += 1){
+for (let index = 0; index < 3; index += 1) {
     const tagElement = document.createElement('h3');
     tagElement.innerHTML = 'Show' + index;
+    tagElement.className = 'description';
     elementMain.appendChild(tagElement);
 }
+
+const sectionleftcontent = document.getElementsByClassName('left-content')[0];
+elementMain.removeChild(sectionleftcontent);
+
+const sectionRightContent = document.getElementsByClassName('right-content')[0];
+sectionRightContent.style.marginRight = 'auto';
+
+const sectionCenterContent = document.getElementsByClassName('center-content')[0];
+sectionCenterContent.parentNode.style.backgroundColor = 'green';
+
+const ul = document.getElementsByTagName('ul')[0];
+      ul.lastChild.remove();
+      ul.lastChild.remove();
+
+
+
+
+
+
+
 
 
 
