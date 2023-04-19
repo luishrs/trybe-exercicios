a9e7030ec01fe9e86ac6269da6392195954326a9
luishrs <luis-hrs@outlook.com>
2023-04-19 10:50:24 -0300

    adicionado pastas para próximo módulo

--- Testes/testes2.js
+++ Testes/testes2.js
@@ -1,8 +1,6 @@
-const sum = (value1, value2) => {
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-      throw new Error('Os valores devem ser numéricos');
-    }
-    return value1 + value2;
-  };
-  
-  console.log(sum(2, '3'));
+const teste = {
+  nome: 'luis',
+  sobrenome: 'silva,'
+}
+teste.nome = 'fabricio';
+console.log(teste);
